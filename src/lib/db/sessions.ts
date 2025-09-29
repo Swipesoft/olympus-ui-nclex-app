34040c0a82e00811fe4f619ed8895821cc93411f
Swipesoft <emmyfine02@gmail.com>
2025-09-29 14:37:43 +0100

    sessions api typescript mismatch fixes

--- src/lib/db/sessions.ts
+++ src/lib/db/sessions.ts
@@ -1,4 +1,6 @@
-import clientPromise  from "@/lib/mongodb"
+// lib/db/sessions.ts
+import clientPromise  from "@/lib/mongodb";
+import { DbSessionResult } from "@/lib/adapters/sessionAdapter"
 
 
 const dbName = 'olympus_users_cloud'; 
@@ -6,14 +8,14 @@ const collectionName = 'user_sessions';
 //const uri = process.env.MONGODB_URI!; 
 
 
-export async function getUserSessions(clerkId: string) {
+export async function getUserSessions(clerkId: string): Promise<DbSessionResult[]> {
     const client = await clientPromise;
     const db = client.db(dbName);
     const collection = db.collection(collectionName);
 
     // Fetch sessions for the given clerkId, sorted by most recent
     const sessions = await collection.find({userId: clerkId }).sort({ date: -1 }).toArray();
-    return sessions;
+    return sessions as DbSessionResult [];
 }   
 
 
