69e87ea6fe35edb9ec38c5e18bdebdda2735bcb3
Swipesoft <emmyfine02@gmail.com>
2025-09-30 02:39:10 +0100

    improvements on UI

--- src/components/Helper/scrollTotop.tsx
+++ src/components/Helper/scrollTotop.tsx
@@ -22,7 +22,7 @@ const ScrollToTop = () => {
 
     return<div className="fixed bottom-4 animate-pulse right-4 cursor-pointer">
         {isVisible && (
-            <button className="bg-pink-600 text-white rounded-full w-12 h-12 flex items-center justify-center focus:outline-none cursor-pointer" onClick={scrollToTop}><FaArrowUp/></button>
+            <button className="bg-indigo-600 text-white rounded-full w-12 h-12 flex items-center justify-center focus:outline-none cursor-pointer" onClick={scrollToTop}><FaArrowUp/></button>
         )}
     </div>
 }
